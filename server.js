839224c9206a83b020cc4a787a8215ac3e99e3e9
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-03-30 21:49:03 +0200

    test

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require.paths.push("./mui/js");
+
 require("http").createServer(function (req,res) {
   res.writeHead(200,{})
-  res.end("testing!")
-}).listen(80)
+  res.end("testing2!")
+}).listen(8080)
