90033f04cdd06835d6a635626038545a8765f201
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-03-30 22:12:25 +0200

    update...

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
 require.paths.push("./mui/js");
+// define as module using the xmodule system for
+// module support in both browsers and ssjs
+require("xmodule").def("muiSampleMain",function(){
+
+// bind the main function
+require("mui").setMain(main);
+
+function main(mui) {
+    // sample ui-page
+    mui.showPage(["page", 
+        ["text", "hello world"]]);
+}
 
 
-require("http").createServer(function (req,res) {
-  res.writeHead(200,{})
-  res.end("testing2!")
-}).listen(8080)
+function search(mui) {
+    // indicat that we are loading something
+    mui.loading();
+
+    // jsonp callback
+    mui.callJsonpWebservice("http://opensearch.addi.dk/1.0/", "callback", {
+        action: "search",
+        query: mui.form.query,
+        source: "bibliotekdk",
+        start: "1",
+        stepvalue: "1",
+        outputType: "json"}, function(result) {
+            mui.showPage(["page", 
+                ["text", "Number of hits: ", result.searchResponse.result.hitCount.$],
+                ["button", {"fn": main}, "back to start"]
+                ]);
+        });
+}
+
+});
