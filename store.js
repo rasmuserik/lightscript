fe2d1b3315bf74edcd54acf500a29927e18b33dd
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-07-13 16:02:54 +0200

    todo

---
+++ store.js
@@ -0,0 +1,13 @@
+// # Storage, automatically synchronised with solsort.com/store
+function Store(uniqName, mergeFn) {
+    function set(key, val) {
+        throw 'NOT IMPLEMENTED'
+    }
+    function get(key, val) {
+        throw 'NOT IMPLEMENTED'
+    }
+    return {
+        set: set,
+        get: get
+    };
+}
