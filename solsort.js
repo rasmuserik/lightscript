ce1f58860eeeed221adb4bec68b83c62cb69a56b
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-07-14 22:18:38 +0200

    ...

--- solsort.js
+++ solsort.js
@@ -1,11 +1,13 @@
 solsort = {};
+/*global solsort:true, localStorage:true, window:true, document:true, alert:true, location:true */
 (function() {
+    "use strict";
     solsort.set = function(key, val) {
-        return localStorage.getItem(name);
-    }
+        return localStorage.getItem(key);
+    };
     solsort.get = function(key) {
-        return localStorage.getItem(name);
-    }
+        return localStorage.getItem(key);
+    };
     solsort.getVars = function() {
         // TODO: unencode urlencoding
         var result = {};
@@ -14,7 +16,7 @@ solsort = {};
             result[t[0]] = t[1];
         });
         return result;
-    }
+    };
     solsort.jsonp = function(uri, args, callback, callbackName) {
         // TODO: urlencode args
         // TODO: make reentrant
@@ -31,94 +33,14 @@ solsort = {};
             '"></script>');
     };
     solsort.error = function(err) {
-        alert('Error: ' + err);
         solsort.jsonp('http://solsort.com/clientError', {error: err});
+        alert('Error on solsort.com: \n' + err + '\nSorry, not quite bug free, if you are online, then the error has been reported...');
         throw err;
     };
 
-    solsort.logout = function() {
-        localStorage.removeItem('userId');
-        localStorage.removeItem('userName');
-        loginUI();
-    }
-
-    solsort.login = function(callback) {
-        var user = localStorage.getItem('userId');
-        if(user) {
-            return callback(user);
-        }
-        throw 'not implemented yet';
-    }
-
-    solsort.loginFacebook = function() {
-        localStorage.setItem('logging in', 'facebook');
-        window.location = "https://www.facebook.com/dialog/oauth?client_id=201142456681777&redirect_uri=http://solsort.com/&scope=&response_type=token";
-    }
-
-    solsort.loginGitHub = function() {
-        localStorage.setItem('logging in', 'github');
-        window.location = 'https://github.com/login/oauth/authorize?client_id=cc14f7f75ff01bdbb1e7';
-    }
-
-    solsort.loginGoogle = function() {
-        localStorage.setItem('logging in', 'google');
-        window.location = 'https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&state=&redirect_uri=http://solsort.com/&response_type=token&client_id=500223099774.apps.googleusercontent.com';
-    }
-
-    function loginAs(user, name) {
-        localStorage.setItem('userId', user);
-        localStorage.setItem('userName', name);
-        solsort.jsonp('http://solsort.com/', {user: user, name: name});
-        var loginFromUrl = localStorage.getItem('loginFromUrl');
-        if(loginFromUrl) {
-            localStorage.removeItem('loginFromUrl');
-            window.location = loginFromUrl;
-        }
-    }
-
-    !function(){
-        var loggingIn = localStorage.getItem('logging in');
-        if(loggingIn) {
-            localStorage.removeItem('logging in');
-
-            if(loggingIn === 'github') {
-                solsort.jsonp('http://solsort.com/githubLogin', solsort.getVars(), function(access_token) {
-                    access_token = access_token.replace(/.*access_token=/, '').replace(/&.*/, '');
-                    solsort.jsonp('https://api.github.com/user', {access_token: access_token},
-                        function(data) {
-                            if(data.data.login) {
-                                loginAs('github:' + data.data.login, data.data.name);
-                                loginUI();
-                            };
-                        });
-                });
-            }
-
-            if(loggingIn === 'facebook') {
-                var access_token = location.hash.replace(/.*access_token=/, '').replace(/&.*/, '');
-                solsort.jsonp('https://graph.facebook.com/me', {access_token: access_token}, function(data) {
-                    if(data.id) { 
-                        loginAs('facebook:' + data.id, data.name);
-                        loginUI();
-                    }
-                });
-            }
-
-            if(loggingIn === 'google') {
-                var access_token = location.hash.replace(/.*access_token=/, '').replace(/&.*/, '');
-                solsort.jsonp('https://www.googleapis.com/oauth2/v1/userinfo', {access_token: access_token}, function(data) {
-                    if(data.id) { 
-                        loginAs('google:' + data.id, data.name);
-                        loginUI();
-                    }
-                });
-            }
-        }
-    }();
-
     function loginUI() {
-        var loginElem = document.getElementById('solsortLogin');
-        if(loginElem) {
+        var solsortLogin= document.getElementById('solsortLogin');
+        if(solsortLogin) {
             var userId = localStorage.getItem('userId');
             var userName = localStorage.getItem('userName');
             if(!userId) {
@@ -134,6 +56,87 @@ solsort = {};
                     '</a>';
             }
         }
+    }
+    solsort.logout = function() {
+        localStorage.removeItem('userId');
+        localStorage.removeItem('userName');
+        loginUI();
     };
+
+    solsort.login = function(callback) {
+        var user = localStorage.getItem('userId');
+        if(user) {
+            return callback(user);
+        }
+        throw 'not implemented yet';
+    };
+
+    solsort.loginFacebook = function() {
+        localStorage.setItem('logging in', 'facebook');
+        window.location = "https://www.facebook.com/dialog/oauth?client_id=201142456681777&redirect_uri=http://solsort.com/&scope=&response_type=token";
+    };
+
+    solsort.loginGitHub = function() {
+        localStorage.setItem('logging in', 'github');
+        window.location = 'https://github.com/login/oauth/authorize?client_id=cc14f7f75ff01bdbb1e7';
+    };
+
+    solsort.loginGoogle = function() {
+        localStorage.setItem('logging in', 'google');
+        window.location = 'https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&state=&redirect_uri=http://solsort.com/&response_type=token&client_id=500223099774.apps.googleusercontent.com';
+    };
+
+    function loginAs(user, name) {
+        localStorage.setItem('userId', user);
+        localStorage.setItem('userName', name);
+        solsort.jsonp('http://solsort.com/', {user: user, name: name});
+        var loginFromUrl = localStorage.getItem('loginFromUrl');
+        if(loginFromUrl) {
+            localStorage.removeItem('loginFromUrl');
+            window.location = loginFromUrl;
+        }
+    }
+
+    (function(){
+        var loggingIn = localStorage.getItem('logging in');
+        var access_token;
+        if(loggingIn) {
+            localStorage.removeItem('logging in');
+
+            if(loggingIn === 'github') {
+                solsort.jsonp('http://solsort.com/githubLogin', solsort.getVars(), function(access_token) {
+                    access_token = access_token.replace(/.*access_token=/, '').replace(/&.*/, '');
+                    solsort.jsonp('https://api.github.com/user', {access_token: access_token},
+                        function(data) {
+                            if(data.data.login) {
+                                loginAs('github:' + data.data.login, data.data.name);
+                                loginUI();
+                            }
+                        });
+                });
+            }
+
+            if(loggingIn === 'facebook') {
+                access_token = location.hash.replace(/.*access_token=/, '').replace(/&.*/, '');
+                solsort.jsonp('https://graph.facebook.com/me', {access_token: access_token}, function(data) {
+                    if(data.id) { 
+                        loginAs('facebook:' + data.id, data.name);
+                        loginUI();
+                    }
+                });
+            }
+
+            if(loggingIn === 'google') {
+                access_token = location.hash.replace(/.*access_token=/, '').replace(/&.*/, '');
+                solsort.jsonp('https://www.googleapis.com/oauth2/v1/userinfo', {access_token: access_token}, function(data) {
+                    if(data.id) { 
+                        loginAs('google:' + data.id, data.name);
+                        loginUI();
+                    }
+                });
+            }
+        }
+    })();
+
     loginUI();
 })();
