1fe9900b609234f2e3bb03878e1c6c395f9c0626
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-07-15 11:00:55 +0200

    ...

--- store.js
+++ store.js
@@ -24,11 +24,26 @@ if(!solsort) {
         xhr.addEventListener("error", error);
         xhr.addEventListener("abort", error);
         xhr.send();
-        
-        
-
     }
     solsort.storeGet = function(store, key, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://solsort.com/store?store=' + store + '&key=' + key);
+
+        function done(a) {
+            try {
+                callback(null, JSON.parse(xhr.responseText));
+            } catch(e) {
+                callback(e || 'xhr-parsing-error', a);
+            }
+        }
+        function error(e) {
+            callback(e || 'xhr-error');
+        }
+
+        xhr.addEventListener("load", done);
+        xhr.addEventListener("error", error);
+        xhr.addEventListener("abort", error);
+        xhr.send();
     }
     solsort.storePost = function(store, key, val, prevVal, callback) {
     }
