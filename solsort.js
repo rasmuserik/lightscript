59a6d707cc7a4e2a23d956065b5afa0210732c98
Rasmus Erik <github-rasmuserik@solsort.dk>
2012-09-20 12:16:37 +0200

    publish-script-in-solsort(+nginx)

--- solsort.js
+++ solsort.js
@@ -1158,3 +1158,16 @@ def("web", function(exports) {
         exports.loadJS("http://solsort.com/store.js");
     };
 });
+// publish web sites {{{1
+def("publish", function(exports) {
+    exports.nodemain = function() {
+        console.log('copying sites to /usr/share/nginx/www/');
+        require('child_process').exec('cp -a sites/* /usr/share/nginx/www/', function(err,stdout,stderr) {
+            console.log('done', stdout.toString(), stderr.toString());
+
+                                                     if(err) {
+                                                     console.log('Error:', err);
+                                                     }
+        });
+    }
+});
